2a94e66cf6874ffdf612cf0876cbe7ad265bbb95
Nobo Bhowmik <nobonita.bhowmik@outlook.com>
2018-02-16 10:22:43 -0500

    include projectiles

---
+++ Assets/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager: MonoBehaviour
+{
+    public static GameManager instance;         
+    public Text scoreText;                    
+    public GameObject gameOvertext;             
+
+    private int score = 0;                      
+    public bool gameOver = false;
+    //public float scrollSpeed = -1.5f;
+
+    public KeyCode restartKey = KeyCode.R;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+       if (gameOver && Input.GetMouseButtonDown(0))
+        {
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+       }
+
+        if (Input.GetKey(restartKey))
+        {
+            SceneManager.LoadScene("main");
+        }
+    }
+
+    public void PlayerScored()
+    {
+        if (gameOver)
+            return;
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void PlayerDied()
+    {
+        gameOvertext.SetActive(true);
+        gameOver = true;
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Player: MonoBehaviour
+{
+    public float upForce;
+    private bool isDead = false;
+
+    public KeyCode fireKey = KeyCode.Space;
+    //public KeyCode upKey = KeyCode.W;
+    //public KeyCode downKey = KeyCode.S;
+
+    public GameObject proj;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetKey(fireKey))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+
+            if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newProjectileObj = Instantiate(proj);
+            newProjectileObj.transform.position = transform.position;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        GameManager.instance.PlayerDied();
+    }
+}
