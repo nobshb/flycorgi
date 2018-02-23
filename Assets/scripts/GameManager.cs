e51c0e0123a3c991a51ee1fc67768afba623476a
Nobo Bhowmik <nobonita.bhowmik@outlook.com>
2018-02-22 19:51:02 -0500

    initial score additions
    adding scores, enemy (fire)

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 using UnityEngine.SceneManagement;
 
 public class GameManager: MonoBehaviour
 {
-    public static GameManager instance;         
+    public static GameManager instance = null;         
     public Text scoreText;                    
     public GameObject gameOvertext;             
 
-    private int score = 0;                      
+    private int score = 0;
+    public int highScore = 0;
+
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
 
@@ -19,15 +22,26 @@ public class GameManager: MonoBehaviour
     void Awake()
     {
         if (instance == null)
+        {
             instance = this;
-        else if (instance != this)
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
             Destroy(gameObject);
+            return;
+        }
+
+        string fullFilePath = Application.persistentDataPath + Path.DirectorySeparatorChar + "SaveData.txt";
+        if (File.Exists(fullFilePath))
+        {
+            string highScoreString = File.ReadAllText(fullFilePath);
+            highScore = int.Parse(highScoreString);
+        }
     }
 
     void Update()
     {
-
-
         if (Input.GetKey(restartKey))
         {
             SceneManager.LoadScene("main");
@@ -46,5 +60,16 @@ public class GameManager: MonoBehaviour
     {
         gameOvertext.SetActive(true);
         gameOver = true;
+    }
+    public void endGame()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+
+            string fullFilePath = Application.dataPath + Path.DirectorySeparatorChar + "SaveData.txt";
+            File.WriteAllText(fullFilePath, highScore.ToString());
+        }
+        SceneManager.LoadScene("GameOverScene");
     }
 }
---
+++ Assets/scripts/ScoreText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScoreText : MonoBehaviour {
+
+    Text _ScoreText;
+
+    // Use this for initialization
+    void Start () {
+        _ScoreText = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _ScoreText.text = "Score: " + GameManager.instance.score + " Best: " + GameManager.instance.highScore;
+    }
+}
